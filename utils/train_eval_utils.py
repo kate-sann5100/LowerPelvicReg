ac3818acb54c9daa6377d1d3869cb36dbfdb02a3
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2022-12-08 12:03:33 +0000

    debug

--- utils/train_eval_utils.py
+++ utils/train_eval_utils.py
@@ -79,5 +79,8 @@ def overwrite_save_dir(args, save_dir):
     if os.path.exists(f"{save_dir}/best_ckpt.pth"):
         if args.overwrite:
             shutil.rmtree(save_dir)
+            os.mkdir(save_dir)
         else:
             raise ValueError(f"already exists: {save_dir}")
+    else:
+        os.mkdir(save_dir)
