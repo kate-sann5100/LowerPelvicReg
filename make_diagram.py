c80b463b148fcbe15c51816433499b096162f692
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-06-14 14:11:19 +0100

    debug

--- make_diagram.py
+++ make_diagram.py
@@ -316,7 +316,7 @@ def slicer():
 
 
 if __name__ == '__main__':
-    crop_visulisation()
-    # main()
+    # crop_visulisation()
+    main()
     # ddf = fake_ddf()
     # plot_ddf(ddf, "make_diagram/fake_ddf.pdf")
