bf3b2ec072a27c26efbc34b0167bb00b04d43e94
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-07-07 14:31:48 +0100

    debug

--- test/test_aug.py
+++ test/test_aug.py
@@ -80,7 +80,7 @@ def test_aug(moving_batch, ddf, aug_multiplier, cut_ratio, args):
     # transform ddf
     affine_ddf = aug_fixed_batch["affine_ddf"]  # (B, 3, W, H, D)
     cut_mask = aug_moving_batch["cut_mask"]  # (B, 1, W, H, D)
-    aug_ddf = affine_ddf.to(ddf.cuda()) + Warp().cuda()(ddf.cuda(), affine_ddf.to(ddf))  # (B, 3, W, H, D)
+    aug_ddf = affine_ddf.to(ddf.cuda()) + Warp()(ddf.cuda(), affine_ddf.to(ddf.cuda()))  # (B, 3, W, H, D)
     aug_ddf = (1 - cut_mask.to(ddf)) * aug_ddf  # (B, 3, W, H, D)
     print("ddf transformed")
     # warp augmented pair
