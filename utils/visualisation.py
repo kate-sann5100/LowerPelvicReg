833891dcebc7824d7fa1e93551f780c9678c3628
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-08-25 23:19:35 +0100

    debug

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -11,11 +11,13 @@ class Visualisation:
         if not os.path.exists(save_path):
             os.mkdir(save_path)
 
-    def vis(self, moving, fixed, pred):
+    def vis(self, moving, fixed, pred=None, prefix=None):
         affine = np.array([[0.75, 0, 0, 0], [0, 0.75, 0, 0], [0, 0, 2.5, 0], [0, 0, 0, 1]])
         moving_name = moving.pop("name")[0]
         fixed_name = fixed.pop("name")[0]
         description = f"{moving_name}_{fixed_name}"
+        if prefix is not None:
+            description += f"_{prefix}"
         sz = moving["t2w"].shape
 
         # warped_t2w, warped_seg
@@ -25,9 +27,10 @@ class Visualisation:
             "moving_seg": moving["seg"],
             "fixed_t2w": fixed["t2w"],
             "fixed_seg": fixed["seg"],
-            "warped_t2w": pred["t2w"],
-            "warped_seg": pred["seg"],
         }
+        if pred is not None:
+            vis_dict["warped_t2w"] = pred["t2w"]
+            vis_dict["warped_seg"] = pred["seg"]
 
         for k, v in vis_dict.items():
             img = nib.Nifti1Image(
