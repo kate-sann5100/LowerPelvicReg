551a1d289de39695dafd2a48e3cc4ab8f1631a2b
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-08-26 22:28:29 +0100

    extend visualisation for batch_size >1

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -13,11 +13,11 @@ class Visualisation:
 
     def vis(self, moving, fixed, pred=None, prefix=None):
         affine = np.array([[0.75, 0, 0, 0], [0, 0.75, 0, 0], [0, 0, 2.5, 0], [0, 0, 0, 1]])
-        moving_name = moving.pop("name")[0]
-        fixed_name = fixed.pop("name")[0]
-        description = f"{moving_name}_{fixed_name}"
+        moving_name = moving.pop("name")
+        fixed_name = fixed.pop("name")
+        description = [f"{mn}_{fn}" for mn, fn in zip(moving_name, fixed_name)]
         if prefix is not None:
-            description += f"_{prefix}"
+            description = [f"{d}_{prefix}" for d in description]
         sz = moving["t2w"].shape
 
         # warped_t2w, warped_seg
@@ -38,8 +38,9 @@ class Visualisation:
             vis_dict["warped_seg"] = pred["seg"]
 
         for k, v in vis_dict.items():
-            img = nib.Nifti1Image(
-                v[0].reshape(*sz[-3:]).detach().cpu().numpy().astype(dtype=np.float32),
-                affine=affine
-            )
-            nib.save(img, f"{self.save_path}/{description}_{k}.nii")
+            for i, d in enumerate(description):
+                img = nib.Nifti1Image(
+                    v[i].reshape(*sz[-3:]).detach().cpu().numpy().astype(dtype=np.float32),
+                    affine=affine
+                )
+                nib.save(img, f"{self.save_path}/{d}_{k}.nii")
