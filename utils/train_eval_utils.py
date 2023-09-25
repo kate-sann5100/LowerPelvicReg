958090a1c8cc3987634c2bf1b4c5c32f9ba8b1d7
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-09-25 10:49:07 +0100

    debug

--- utils/train_eval_utils.py
+++ utils/train_eval_utils.py
@@ -139,6 +139,7 @@ def load_weight(student, teacher, ckpt, same_init=False):
     student.load_state_dict(ckpt["student"], strict=True)
     for k, v in ckpt.items():
         print(k)
+    print(ckpt["teacher"])
     exit()
     for t_id, t_model in teacher.items():
         t_model.load_state_dict(
