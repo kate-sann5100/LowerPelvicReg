e15241e6c3a8ecdea94aac6175a23556088dd174
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-10-06 11:22:00 +0100

    debug

--- generate_atlas.py
+++ generate_atlas.py
@@ -136,7 +136,7 @@ def update_atlas(atlas, dataloader, model, batch_size, num_samples, args):
     var_seg, avg_seg = torch.var_mean(all_seg, dim=0, keepdim=True)  # (1, 9, W, H, D)
     atlas = {
         "t2w": avg_t2w,
-        "seg": avg_seg,
+        "seg": torch.argmax(avg_seg, dim=1, keepdim=True),
         "var_t2w": var_t2w,
         "var_seg": var_seg
     }
