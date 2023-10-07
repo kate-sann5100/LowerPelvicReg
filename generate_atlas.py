c961d8f8d771ff24569638d9b2c5e49237e2530f
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-10-07 18:39:18 +0100

    debug

--- generate_atlas.py
+++ generate_atlas.py
@@ -135,8 +135,6 @@ def update_atlas(atlas, dataloader, model, batch_size, num_samples, args):
             all_t2w[step*batch_size: step*batch_size+len(img["t2w"])] = binary["t2w"]  # (B, 1, W, H, D)
             all_seg[step*batch_size: step*batch_size+len(img["t2w"])] = binary["seg"]  # (B, 9, W, H, D)
     all_t2w, all_seg = all_t2w.reshape(-1, 1, *args.size), all_seg.reshape(-1, 9, *args.size)
-    print(torch.unique(torch.sum(all_seg, dim=1)))
-    exit()
     var_t2w, avg_t2w = torch.var_mean(all_t2w, dim=0, keepdim=True)   # (1, 3, W, H, D)
     var_seg, avg_seg = torch.var_mean(all_seg, dim=0, keepdim=True)  # (1, 9, W, H, D)
     atlas = {
