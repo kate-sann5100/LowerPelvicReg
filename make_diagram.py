551f5044dd867ff71678e27bfad8cc5bd487b1fc
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-12-01 12:44:10 +0000

    debug

--- make_diagram.py
+++ make_diagram.py
@@ -79,17 +79,17 @@ def warp_ddf(moving, fixed, warped, aug_fixed):
     #     print(k)
     ddf = warped["ddf"]
     augmentation = aug_fixed["affine_ddf"].to(ddf)
-    aug_ddf = augmentation.to(ddf) + Warp()(ddf, augmentation.to(ddf))
+    # aug_ddf = augmentation.to(ddf) + Warp()(ddf, augmentation.to(ddf))
     # plot_ddf(ddf, "make_diagram/ddf.png")
     plot_ddf(augmentation, "make_diagram/augmentation.png")
     plot_ddf(aug_ddf, "make_diagram/warped_ddf.png")
 
     aug_fixed["seg"] = Warp(mode="bilinear")(fixed["seg"], augmentation),
     aug_fixed["name"] = [f"aug_{n}" for n in aug_fixed["name"]]
-    aug_warp = {
-        "t2w": Warp(mode="bilinear")(moving["t2w"], aug_ddf),
-        "seg": Warp(mode="nearest")(moving["seg"], aug_ddf)
-    }
+    # aug_warp = {
+    #     "t2w": Warp(mode="bilinear")(moving["t2w"], aug_ddf),
+    #     "seg": Warp(mode="nearest")(moving["seg"], aug_ddf)
+    # }
 
     warp = {
         "t2w": Warp(mode="bilinear")(moving["t2w"], ddf),
@@ -101,7 +101,7 @@ def warp_ddf(moving, fixed, warped, aug_fixed):
     vis.vis(
         moving=moving,
         fixed=aug_fixed,
-        pred=aug_warp,
+        pred=warp,
     )
     # moving["name"] = moving_name
     # vis.vis(
@@ -236,7 +236,7 @@ def get_data(args):
 
     for ul_moving, ul_fixed, aug_moving, aug_fixed in ul_loader:
         break
-        
+
     cuda_batch(l_moving)
     cuda_batch(l_fixed)
     cuda_batch(ul_moving)
