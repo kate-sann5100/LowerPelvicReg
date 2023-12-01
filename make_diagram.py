2d5887174c0ce797aca920e10801624f238610cf
kate-sann5100 <yiwen.li@st-annes.ox.ac.uk>
2023-12-01 12:45:02 +0000

    debug

--- make_diagram.py
+++ make_diagram.py
@@ -82,7 +82,7 @@ def warp_ddf(moving, fixed, warped, aug_fixed):
     # aug_ddf = augmentation.to(ddf) + Warp()(ddf, augmentation.to(ddf))
     # plot_ddf(ddf, "make_diagram/ddf.png")
     plot_ddf(augmentation, "make_diagram/augmentation.png")
-    plot_ddf(aug_ddf, "make_diagram/warped_ddf.png")
+    # plot_ddf(aug_ddf, "make_diagram/warped_ddf.png")
 
     aug_fixed["seg"] = Warp(mode="bilinear")(fixed["seg"], augmentation),
     aug_fixed["name"] = [f"aug_{n}" for n in aug_fixed["name"]]
